af7e1893f4c7186d7594aeb605fd0fb8e54d6c4d
Simon Schmoll <simon.schmoll@protonmail.com>
2019-12-16 15:46:48 +0100

    Add errorful tests to exercise service

--- server/src/exercises/exercises.service.spec.ts
+++ server/src/exercises/exercises.service.spec.ts
@@ -2,6 +2,8 @@ import {Test, TestingModule} from '@nestjs/testing';
 import {getModelToken} from '@nestjs/mongoose';
 import {ExerciseService} from './exercises.service';
 import {ExerciseMockModel, exerciseMockData} from '../mocks/exercise_mock';
+import {HttpException, HttpStatus} from '@nestjs/common';
+import {Exercise} from './interfaces/exercises';
 
 describe('ExerciseService', () => {
         let exerciseService: ExerciseService;
@@ -30,6 +32,15 @@ describe('ExerciseService', () => {
                 expect(actualResult).toEqual(exerciseMockData[2]);
         });
 
+        it('getExercisesByCategory should throw HttpException when category is not in the defined Array', async () => {
+                try {
+                        expect(await exerciseService.getExercisesByCategory('NotDefined')).rejects.toThrow();
+                } catch (error) {
+                        expect(error).toBeInstanceOf(HttpException);
+                        expect((error as HttpException).getStatus()).toEqual(HttpStatus.UNPROCESSABLE_ENTITY);
+                }
+        });
+
         it('getAllExercises should return all stored exercises', async () => {
                 const actualResult = await exerciseService.getAllExercises();
                 expect(actualResult).toEqual(exerciseMockData)
@@ -38,5 +49,16 @@ describe('ExerciseService', () => {
         it('postExercise should return the input DTO', async () => {
                 const actualResult = await exerciseService.postExercise(exerciseMockData[0]);
                 expect(actualResult).toEqual(exerciseMockData[0])
+        });
+
+        it('postExercise should throw Error when category of dto is not in the defined Array', async () => {
+                try {
+                        const errorfulMockdata = Object.assign({}, exerciseMockData[0], { ...exerciseMockData[0], category: 'NotDefined' });
+                        console.log('errorfulMockData: , ', errorfulMockdata);
+                        expect(await exerciseService.postExercise(errorfulMockdata)).rejects.toThrow();
+                } catch (error) {
+                        expect(error).toBeInstanceOf(HttpException);
+                        expect((error as HttpException).getStatus()).toEqual(HttpStatus.UNPROCESSABLE_ENTITY);
+                }
         });
 });
