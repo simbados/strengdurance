c1f89506d9a60a620e5bb60951d585cfa3471261
Simon Schmoll <simon.schmoll@protonmail.com>
2020-02-19 21:32:30 +0100

    fix exercise controller tests

--- server/src/exercises/exercises.controller.spec.ts
+++ server/src/exercises/exercises.controller.spec.ts
@@ -2,54 +2,58 @@ import {Test, TestingModule} from '@nestjs/testing';
 import {Exercise} from './interfaces/exercises';
 import {ExerciseController} from './exercises.controller';
 import {ExerciseService} from './exercises.service';
-import {ExerciseMockService, exerciseMockData} from '../mocks/exercise_mock';
+import ExerciseMockService from '../mocks/exercise_service_mock';
+import {exerciseMockData} from '../mocks/exercise_mock_data';
 
 describe('Exercise Controller', () => {
-        let controller: ExerciseController;
-        let service: ExerciseService;
+  let controller: ExerciseController;
+  let service: ExerciseService;
+  const userId = 'userId';
+  const request = {user: {userId: userId}};
 
-        beforeEach(async () => {
-                const module: TestingModule = await Test.createTestingModule({
-                        controllers: [ExerciseController],
-                        providers: [{
-                                provide: ExerciseService,
-                                useValue: new ExerciseMockService(),
-                        }]
-                }).compile();
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [ExerciseController],
+      providers: [{
+        provide: ExerciseService,
+        useValue: new ExerciseMockService(),
+      }]
+    }).compile();
 
-                controller = module.get<ExerciseController>(ExerciseController);
-                service = module.get<ExerciseService>(ExerciseService);
-        });
+    controller = module.get<ExerciseController>(ExerciseController);
+    service = module.get<ExerciseService>(ExerciseService);
+  });
 
-        it('should be defined', () => {
-                expect(controller).toBeDefined();
-        });
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
 
-        it('getAllExercises should return all exercises when called', async () => {
-                const spy = jest.spyOn(service, 'getAllExercises').mockImplementation(() => {
-                        return new Promise<Exercise[]>((resolve) => resolve(exerciseMockData))
-                });
-                const actualResponse = await controller.getAllExercises();
-                expect(spy).toBeCalledTimes(1);
-                expect(actualResponse).toEqual(exerciseMockData);
-        })
+  it('getAllExercises should return all exercises when called', async () => {
+    const spy = jest.spyOn(service, 'getAllExercises').mockReturnValueOnce(
+      new Promise<Exercise[]>((resolve) => resolve(exerciseMockData)));
+    const actualResponse = await controller.getAllExercises(request);
+    expect(spy).toBeCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith(userId);
+    expect(actualResponse).toEqual(exerciseMockData);
+  })
 
-        it('postExercise should return provided exercise when called', async () => {
-                const spy = jest.spyOn(service, 'postExercise').mockImplementation(() => {
-                        return new Promise<Exercise>((resolve) => resolve(exerciseMockData[0]))
-                });
-                const actualResponse = await controller.postExercise(exerciseMockData[0]);
-                expect(spy).toBeCalledTimes(1);
-                expect(actualResponse).toEqual(exerciseMockData[0]);
-        })
+  it('postExercise should return provided exercise when called', async () => {
+    const spy = jest.spyOn(service, 'postExercise').mockReturnValueOnce(
+      new Promise(resolve => resolve(exerciseMockData[0])));
+    const actualResponse = await controller.postExercise(request, exerciseMockData[0]);
+    expect(spy).toBeCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith(exerciseMockData[0], userId);
+    expect(actualResponse).toEqual(exerciseMockData[0]);
+  })
 
-        it('getExercisesByCategory should return expected exercises', async () => {
-                const expectedValues: Exercise[] = [exerciseMockData[0], exerciseMockData[1]]
-                const spy = jest.spyOn(service, 'getExercisesByCategory').mockImplementation(() => {
-                        return new Promise<Exercise[]>((resolve) => resolve(expectedValues))
-                });
-                const actualResponse = await controller.getExercisesByCategory('Arms');
-                expect(spy).toBeCalledTimes(1);
-                expect(actualResponse).toEqual(expectedValues);
-        })
+  it('getExercisesByCategory should return expected exercises', async () => {
+    const category = {category: 'Arms'};
+    const expectedValues: Exercise[] = [exerciseMockData[0], exerciseMockData[1]]
+    const spy = jest.spyOn(service, 'getExercisesByCategory').mockReturnValueOnce(
+      new Promise<Exercise[]>(resolve => resolve(expectedValues)));
+    const actualResponse = await controller.getExercisesByCategory(request, category);
+    expect(spy).toBeCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith('Arms', userId);
+    expect(actualResponse).toEqual(expectedValues);
+  })
 });
--- server/src/mocks/exercise_mock_data.ts
+++ server/src/mocks/exercise_mock_data.ts
@@ -1,4 +1,6 @@
 import Category from '../exercises/categories';
+import {userMockData} from '../mocks/user_mock_data';
+import {Exercise} from '../exercises/interfaces/exercises';
 
 const testId: string = 'testId';
 const testUserId: string = 'testUserId';
@@ -8,4 +10,10 @@ const exerciseMockModelData = [
   {name: 'Pushdowns', category: Category[0], user: testUserId},
   {name: 'Bench', category: Category[2], user: testUserId},
 ];
-export {exerciseMockModelData, testId};
+
+const exerciseMockData: Exercise[] = [
+  {name: 'Curls', category: Category[0], user: userMockData},
+  {name: 'Pushdowns', category: Category[0], user: userMockData},
+  {name: 'Bench', category: Category[2], user: userMockData},
+]
+export {exerciseMockModelData, testId, exerciseMockData};
