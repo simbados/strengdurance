2bebf8faf257847c106289d58f5a9d7235469148
Simon Schmoll <simon.schmoll@protonmail.com>
2020-03-03 12:36:37 +0100

    Add frontend to CI and modify docker images

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from './validation/validation.pipes'
+import { ValidationPipe } from './validation/validation.pipes';
 import * as helmet from 'helmet';
 
 declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
+  app.enableCors();
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
